1ff768d56c0ef10f415ff0c8de8765921087ca8c
pavel.kracmar <pavel.kracmar@icloud.com>
2026-09-01 00:14:03 +0200

    saves fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p ./saves
-if [ ! -f ./saves/save.zip ]; then
-    ./factorio/bin/x64/factorio --create ./saves/save.zip
+mkdir -p ./factorio/saves
+if [ ! -f ./factorio/saves/save.zip ]; then
+    ./factorio/bin/x64/factorio --create ./factorio/saves/save.zip
 fi
 exec ./factorio/bin/x64/factorio --start-server-load-latest --server-settings ./factorio/data/server-settings.example.json
