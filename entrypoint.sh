102cae6fd19d79571582b7c31e1c6151b71e1cbd
pvlkrc <pavel.kracmar@icloud.com>
2026-09-04 15:55:08 +0200

    server-settings.json fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ mkdir -p ./factorio/saves
 if [ ! -f ./factorio/saves/save.zip ]; then
     ./factorio/bin/x64/factorio --create ./factorio/saves/save.zip
 fi
-exec ./factorio/bin/x64/factorio --start-server-load-latest --server-settings ./factorio/data/server-settings.example.json
+exec ./factorio/bin/x64/factorio --start-server-load-latest --server-settings ./factorio/data/server-settings.json
